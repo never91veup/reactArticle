e419d6eeb29a060bee7affba43febc355b82e1cc
never91veup <vp.vlad00@mail.ru>
2022-09-15 23:08:25 +0300

    added full

---
+++ src/components/AppRouter.jsx
@@ -0,0 +1,44 @@
+import React, {useContext} from 'react';
+import {Redirect, Route, Switch} from "react-router-dom";
+import {publicRoutes, privateRoutes} from "../router/routes";
+import {AuthContext} from "../context/context";
+import Loader from "./UI/Loader/Loader";
+
+const AppRouter = () => {
+    const {isAuth, isLoading} = useContext(AuthContext);
+
+    if (isLoading) {
+        return <Loader />
+    }
+
+    return (
+        isAuth
+            ?
+            <Switch>
+                {privateRoutes.map(route =>
+                    <Route
+                        key={route.path}
+                        component={route.component}
+                        path={route.path}
+                        exact={route.exact}
+                    />
+                )}
+                <Redirect to='/posts' />
+            </Switch>
+            :
+            <Switch>
+                {publicRoutes.map(route =>
+                    <Route
+                        key={route.path}
+                        component={route.component}
+                        path={route.path}
+                        exact={route.exact}
+                    />
+                )}
+                <Redirect to='/login' />
+            </Switch>
+
+    );
+};
+
+export default AppRouter;
